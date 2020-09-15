61cb2f24ab9075095eb3f1d2f5de18fd0efbc460
MhRantz <meganrantz@gmail.com>
2020-09-15 13:03:40 -0400

    get random set of characters

--- js/index.js
+++ js/index.js
@@ -3,10 +3,28 @@ const icon = {};
 
 icon.chars = ['a','b','c','d','e'];
 
-icon.random 
+//setting lengths
+icon.handSize = 5;
+
+//containers for hands
+icon.hand1 = [];
+icon.hand2 = [];
+
+//get random number between 0 and n
+icon.random = function(n){
+	return Math.floor(Math.random() * n)
+}
+
+//deal hand
+icon.dealPlayer = function(handNum, n){
+	for(let i = 0; i < icon.handSize; i++){
+		handNum.push(icon.chars[icon.random(n)])
+	}
+}
 
 icon.init = function(){
-	console.log('cool');
+	console.log(icon.hand1);
+	icon.dealPlayer(icon.hand1, icon.handSize);
 }
 
 document.addEventListener("DOMContentLoaded", function(){
