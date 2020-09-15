22374f20b9aa69857d309372ac22672769ac3c3c
MhRantz <meganrantz@gmail.com>
2020-09-15 12:23:15 -0400

    initial

---
+++ js/index.js
@@ -0,0 +1,14 @@
+
+const icon = {};
+
+icon.chars = ['a','b','c','d','e'];
+
+icon.random 
+
+icon.init = function(){
+	console.log('cool');
+}
+
+document.addEventListener("DOMContentLoaded", function(){
+  icon.init();
+});
